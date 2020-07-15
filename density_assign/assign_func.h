e15ca9f96cc1a94423fc285471f939c989836332
Prem Vijay V <35184933+premvijay@users.noreply.github.com>
2020-07-16 02:22:15 +0530

    implimenting in C is in process

---
+++ density_assign/assign_func.h
@@ -0,0 +1,12 @@
+#ifndef ASSIGN_FUNC_H_INCLUDED
+#define ASSIGN_FUNC_H_INCLUDED
+
+float W_cic(float *diff){
+return (1.0 - diff);
+};
+
+float W_ngp(float diff){
+return 1;
+};
+
+#endif // ASSIGN_FUNC_H_INCLUDED
---
+++ density_assign/cic.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include "assign_func.h"
+
+float arr[] = {0.1,0.3,0.7};
+
+int main(){
+    std::cout << W_cic(arr);
+    return 0;
+}
